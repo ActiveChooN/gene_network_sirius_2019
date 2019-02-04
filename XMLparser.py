236a6f943f73d8cb32e4f00d5a2856a7e0c49a57
ogarokpeter <ogarokpeter@gmail.com>
2019-02-04 16:49:46 +0300

    parser of XML graph

---
+++ XMLparser.py
@@ -0,0 +1,24 @@
+import xml.dom.minidom as md
+import numpy as np
+
+
+def xml_graph_to_adjacency_matrix(filename):
+    dom = md.parse(filename)
+
+    # print(dom.toprettyxml())
+
+    nodes = dom.getElementsByTagName("Node")
+    ids = [int(a.getAttribute('id')) for a in nodes]
+    # print(ids)
+    adjacency_matrix = np.zeros(shape=(len(ids), len(ids)))
+    edges = dom.getElementsByTagName("Edge")
+    for e in edges:
+        source = int(e.getElementsByTagName('from')[0].firstChild.nodeValue)
+        target = int(e.getElementsByTagName('to')[0].firstChild.nodeValue)
+        adjacency_matrix[source][target] = 1
+        adjacency_matrix[target][source] = 1
+    print(adjacency_matrix)
+
+
+filename = '/home/user/Sirius/XMLparser/Graph.xml'
+xml_graph_to_adjacency_matrix(filename)
